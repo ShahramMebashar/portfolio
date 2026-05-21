7c85f4f1e31573313d6868ffa7273a738a22d396
Shahram Mebashar <shahram.webdev@gmail.com>
2026-05-21 23:58:00 +0300

    feat: update avatar image source and adjust styling in Home component

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "media.licdn.com",
+        hostname: "cdn.resume.krd",
         pathname: "/**",
       },
     ],
